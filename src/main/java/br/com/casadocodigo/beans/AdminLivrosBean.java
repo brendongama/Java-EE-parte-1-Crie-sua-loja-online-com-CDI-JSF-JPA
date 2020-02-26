89d467b17e9e83bee504c6b0ce5f3670e0912fef
brend <brend@DESKTOP-KNMQNA0.Home>
2020-02-26 20:32:35 -0300

    Salvando os Autores

--- src/main/java/br/com/casadocodigo/beans/AdminLivrosBean.java
+++ src/main/java/br/com/casadocodigo/beans/AdminLivrosBean.java
@@ -1,8 +1,16 @@
 package br.com.casadocodigo.beans;
 
-import javax.enterprise.context.RequestScoped;
-import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.transaction.Transactional;
+
+import br.com.casadocodigo.daos.AutorDao;
+import br.com.casadocodigo.daos.LivroDAO;
+import br.com.casadocodigo.models.Autor;
 import br.com.casadocodigo.models.Livro;
 
 @Named
@@ -11,8 +19,26 @@ public class AdminLivrosBean {
 
 	private Livro livro = new Livro();
 	
+	@Inject
+	private LivroDAO dao;
+	@Inject
+	private AutorDao autorDao;
+	
+	private List<Integer> autoresId = new ArrayList<Integer>();
+	
+	@Transactional
 	public void salvar() {
+		for(Integer autorId: autoresId) {
+			livro.getAutores().add(new Autor(autorId));
+		}
+		dao.salvar(livro);
 		System.out.println("Livro Cadastrado: " + livro);
+		this.livro = new Livro();
+		this.autoresId = new ArrayList<>();
+	}
+	
+	public List<Autor> getAutores() {
+		 return autorDao.listar();
 	}
 
 	public Livro getLivro() {
@@ -21,5 +47,21 @@ public class AdminLivrosBean {
 
 	public void setLivro(Livro livro) {
 		this.livro = livro;
+	}
+
+	public LivroDAO getDao() {
+		return dao;
+	}
+
+	public void setDao(LivroDAO dao) {
+		this.dao = dao;
+	}
+
+	public List<Integer> getAutoresId() {
+		return autoresId;
+	}
+
+	public void setAutoresId(List<Integer> autoresId) {
+		this.autoresId = autoresId;
 	}
 }
--- src/main/java/br/com/casadocodigo/models/Livro.java
+++ src/main/java/br/com/casadocodigo/models/Livro.java
@@ -1,12 +1,15 @@
 package br.com.casadocodigo.models;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
 
 
 @Entity
@@ -22,6 +25,15 @@ public class Livro {
 	private BigDecimal preco;
 	private Integer numeroPaginas;
 	
+	@ManyToMany
+	private List<Autor> autores = new ArrayList<Autor>();
+	
+	public List<Autor> getAutores() {
+		return autores;
+	}
+	public void setAutores(List<Autor> autores) {
+		this.autores = autores;
+	}
 	public String getTitulo() {
 		return titulo;
 	}
@@ -46,12 +58,12 @@ public class Livro {
 	public void setNumeroPaginas(Integer numeroPaginas) {
 		this.numeroPaginas = numeroPaginas;
 	}
-	
 	@Override
 	public String toString() {
-		return "Livro [titulo=" + titulo + ", descricao=" + descricao + ", preco=" + preco + ", numeroPaginas="
-				+ numeroPaginas + "]";
+		return "Livro [id=" + id + ", titulo=" + titulo + ", descricao=" + descricao + ", preco=" + preco
+				+ ", numeroPaginas=" + numeroPaginas + ", autores=" + autores + "]";
 	}
 	
+
 	
 }
