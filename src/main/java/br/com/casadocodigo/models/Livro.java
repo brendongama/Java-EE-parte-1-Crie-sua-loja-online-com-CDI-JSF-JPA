a3d58502243d2526ab487d0bf54425db488d1319
brend <brend@DESKTOP-KNMQNA0.Home>
2020-03-04 22:17:05 -0300

    Usando a Bean Validation

--- src/main/java/br/com/casadocodigo/models/Livro.java
+++ src/main/java/br/com/casadocodigo/models/Livro.java
@@ -10,6 +10,13 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.ManyToMany;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
 
 
 @Entity
@@ -17,15 +24,25 @@ public class Livro {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;	
+	private Integer id;
+	
+	@NotBlank
 	private String titulo;
 	
 	@Lob
+	@Length(min=10)
+	@NotBlank
 	private String descricao;
+	
+	@DecimalMin("20")
 	private BigDecimal preco;
+	
+	@Min(50)
 	private Integer numeroPaginas;
 	
 	@ManyToMany
+	@Size(min=1)
+	@NotNull
 	private List<Autor> autores = new ArrayList<Autor>();
 	
 	public List<Autor> getAutores() {
